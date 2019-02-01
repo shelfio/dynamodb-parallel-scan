8bc98d1639f4d068c801d3fb42bfd8b300517ed3
Vlad Holubiev <golubev.vld@gmail.com>
2019-02-01 21:38:17 +0200

    test: cover case to return 3 out of 10 items

--- src/index.test.js
+++ src/index.test.js
@@ -5,13 +5,13 @@ describe('parallelScan', () => {
   const files = [
     {id: 'some-file-id-1'},
     {id: 'some-file-id-2'},
-    {id: 'some-file-id-3'},
+    {id: 'some-file-id-3', fileSize: 100},
     {id: 'some-file-id-4'},
     {id: 'some-file-id-5'},
-    {id: 'some-file-id-6'},
+    {id: 'some-file-id-6', fileSize: 200},
     {id: 'some-file-id-7'},
     {id: 'some-file-id-8'},
-    {id: 'some-file-id-9'},
+    {id: 'some-file-id-9', fileSize: 300},
     {id: 'some-file-id-10'}
   ];
 
@@ -26,8 +26,7 @@ describe('parallelScan', () => {
         FilterExpression: 'attribute_exists(#id)',
         ExpressionAttributeNames: {
           '#id': 'id'
-        },
-        ProjectionExpression: 'id'
+        }
       },
       {concurrency: 1}
     );
@@ -46,13 +45,31 @@ describe('parallelScan', () => {
         FilterExpression: 'attribute_exists(#id)',
         ExpressionAttributeNames: {
           '#id': 'id'
-        },
-        ProjectionExpression: 'id'
+        }
       },
       {concurrency: 1}
     );
 
     expect(items).toHaveLength(10);
+
+    for (let item of items) {
+      expect(files).toContainEqual(item);
+    }
+  });
+
+  it('should return 3 items with concurrency 50', async () => {
+    const items = await parallelScan(
+      {
+        TableName: 'files',
+        FilterExpression: 'attribute_exists(#fileSize)',
+        ExpressionAttributeNames: {
+          '#fileSize': 'fileSize'
+        }
+      },
+      {concurrency: 1}
+    );
+
+    expect(items).toHaveLength(3);
 
     for (let item of items) {
       expect(files).toContainEqual(item);
